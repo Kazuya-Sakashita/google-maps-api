51502cfaa07a8b16883c5ab27bcc7d5d00be496b
Kazuya-Sakashita <kz0508@gmail.com>
2024-03-08 07:53:50 +0900

    投稿機能追加

---
+++ db/migrate/20240307224822_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
